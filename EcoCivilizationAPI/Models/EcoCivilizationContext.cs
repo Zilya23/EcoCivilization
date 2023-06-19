4cf5220ac909b6fc736ffeef811dccc4b9c30bfa
ZilyaZavr <uasuna@inbox.ru>
2023-06-19 14:29:27 +0300

    server api

--- EcoCivilizationAPI/Models/EcoCivilizationContext.cs
+++ EcoCivilizationAPI/Models/EcoCivilizationContext.cs
@@ -31,7 +31,7 @@ public partial class EcoCivilizationContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-BLMHDHN\\SQL;Database=EcoCivilization;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=194.67.111.16;Database=EcoCivilization;TrustServerCertificate=True;User Id=sa;Password=Password123!;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
